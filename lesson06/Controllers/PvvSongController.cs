4cf7d246c6f79dc2da668f3bd3095d946b4c4804
phan viet vuong <phanvietvuong0601@gmail.com>
2024-05-29 11:00:10 +0700

    lesson+lap5

---
+++ lesson06/Controllers/PvvSongController.cs
@@ -0,0 +1,29 @@
+﻿using lesson06.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace lesson06.Controllers
+{
+    public class PvvSongController : Controller
+    {
+        private static List<PvvSong> pvvSongs = new List<PvvSong>()
+        {
+          new PvvSong{ Id = 221090085, PvvTitle = "phan viet vuong", PvvAuthor = "k22cntt3", PvvArtist = "NTU", PvvYearRelease="2020"},
+          new PvvSong{ Id = 1, PvvTitle = "phan viet vuong", PvvAuthor = "k22cnt3", PvvArtist = "NTU", PvvYearRelease="2020"}
+        };
+
+        // GET: PvvSong
+        public ActionResult PvvIndex()
+        {
+            return View(pvvSongs);
+        }
+        public ActionResult PvvCreate() 
+        {
+            var pvvsong = new PvvSong();
+            return View(pvvsong);
+        }
+    }
+}
---
+++ lesson06/Models/PvvSong.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace lesson06.Models
+{
+    public class PvvSong
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage ="Pvv:hay nhap tieu de")]
+        [DisplayName("tieu de")]
+        public string PvvTitle { get; set; }
+        [Required(ErrorMessage ="Pvv:hay nhap tac gia")]
+        [DisplayName("tac gia")]
+
+        public string PvvAuthor { get; set; }
+        [Required(ErrorMessage ="Pvv:hay nhap nghe si")]
+        [StringLength(50,MinimumLength =2, ErrorMessage = "Pvv:ten nghe si toi thieu 2 ki tu,toi da 50 ky tu")]
+        [DisplayName("nghe si")]
+
+        public string PvvArtist { get; set; }
+        [Required(ErrorMessage ="Pvv:hay nhap nam xuat ban")]
+        [RegularExpression(@"[0-9]{4}", ErrorMessage = "Pvv:nhap xuat ban phai co 2 ky tu so ")]
+        [Range(1900,2024, ErrorMessage = "Pvv:nam xuat ban khoang 1900-2024")]
+        public string PvvYearRelease { get; set; }
+    }
+}
